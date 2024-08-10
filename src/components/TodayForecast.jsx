567ebd9c73ca48b5cc836364a4b084de9da6f375
behnam nabati <behnamnabati0@gmail.com>
2024-08-11 00:38:07 +0200

    swiper.js added to th project to create an slider for Today forecast

--- src/components/TodayForecast.jsx
+++ src/components/TodayForecast.jsx
@@ -1,7 +1,14 @@
-import React, { useMemo } from 'react';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css'; // Import Swiper styles
+import 'swiper/css/navigation'; // Import Swiper navigation styles
 import styles from './Forecast.module.css';
 import { getWeatherIcon } from '../helper/helper';
 import { getTodayForecast } from '../helper/helper';
+import LeftArrow from '/images/left-arrow.png'
+import RightArrow from '/images/arrow-right.png'
+
 
 function TodayForecast({ forecast }) {
     const todayForecast = getTodayForecast(forecast);
@@ -9,22 +16,45 @@ function TodayForecast({ forecast }) {
     return (
         <div className={styles.todayForecastContainer}>
             <h2>24-hour Forecast</h2>
-            <div className={styles.todayForecastGrid}>
+            <div className={styles.navigationContainer}>
+                <button className={`swiper-button-next ${styles.customNext}`}><img src={RightArrow} alt='' /></button>
+                <button className={`swiper-button-prev ${styles.customPrev}`}><img src={LeftArrow} alt='' /></button>
+            </div>
+            <Swiper
+                spaceBetween={10}
+                slidesPerView={1}
+                navigation={{ prevEl: '.swiper-button-prev', nextEl: '.swiper-button-next' }}
+                modules={[Navigation]}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 1,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                    },
+                }}
+            >
                 {todayForecast.map((data, index) => {
                     const date = new Date(data.dt * 1000);
-                    const dateString = date.toLocaleDateString([], { month: '2-digit', day: '2-digit' }); // Format date as MM/DD
-                    const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // Format time
+                    const dateString = date.toLocaleDateString([], { month: '2-digit', day: '2-digit' });
+                    const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
                     return (
-                        <div key={index} className={styles.forecastItem}>
-                            <p>{timeString}</p>
-                            <img src={getWeatherIcon(data.weather[0].description)} alt={forecast.list[0].weather[0].description} />
-                            <p>{data.main.temp.toFixed(0)} °C</p>
-                            <p style={{color : "#b7b7b7", fontSize : "0.9rem"}}>{dateString}</p>
-                        </div>
+                        <SwiperSlide key={index}>
+                            <div className={styles.todayForecastItem}>
+                                <p>{timeString}</p>
+                                <img src={getWeatherIcon(data.weather[0].description)} alt={forecast.list[0].weather[0].description} />
+                                <p>{data.main.temp.toFixed(0)} °C</p>
+                                <p style={{ color: "#b7b7b7", fontSize: "0.9rem" }}>{dateString}</p>
+                            </div>
+                        </SwiperSlide>
                     );
                 })}
-            </div>
+            </Swiper>
         </div>
     );
 }
